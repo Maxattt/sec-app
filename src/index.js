f4ed256e40ccd93fb7dc640e4143de35a4d83bd7
Maxattt <zhumyrtas@list.ru>
2025-03-05 07:53:14 +0500

    added person details

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app';
 
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
