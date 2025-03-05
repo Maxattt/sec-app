a7ac61486f068b7b8be2484751a9d074ae032ba0
Maxattt <zhumyrtas@list.ru>
2025-03-05 08:35:17 +0500

    added error catching in components

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -10,14 +10,14 @@ export default class ItemList extends Component {
   swapiService = new SwapiService()
 
     state = {
-        peopleList: []
+        peopleList: null
     }
 
     componentDidMount() {
         this.swapiService
             .getAllPeople()
             .then((peopleList) => {
-                this.setState({peopleList})
+                this.setState({ peopleList })
             })
     }
 
