8cfaaab0b98042a5440795723e421e52b4845b54
Maxattt <zhumyrtas@list.ru>
2025-03-12 05:54:50 +0500

    added error catcher component

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
-import ErrorIndicator from '../error-indicator/error-indicator';
 
 import './people-page.css';
 import SwapiService from "../../services/swapi-service";
 import Row from "../Row";
+import ErrorBoundary from "../error-boundary";
 
 export default class PeoplePage extends Component {
 
@@ -14,13 +14,6 @@ export default class PeoplePage extends Component {
 
     state = {
         selectedPerson: null,
-        hasError: false
-    }
-
-    componentDidCatch(error, info) {
-        this.setState({
-            hasError: true
-        })
     }
 
     onPersonSelected = (selectedPerson) => {
@@ -28,18 +21,10 @@ export default class PeoplePage extends Component {
     };
 
     render() {
-
-        if (this.state.hasError) {
-            return <ErrorIndicator />
-        }
-
         const itemList = (
-            <ItemList
-                onItemSelected={this.onPersonSelected}
-                getData={ this.swapiService.getAllPeople }
-                renderItem={({ name, gender, birthYear}) => (
-                    `${name} (${gender}, ${birthYear})`
-                )} />
+            <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople}>
+                { i => `${i.name} (${i.birthYear})` }
+            </ItemList>
         )
 
         const personDetails = (
@@ -47,7 +32,9 @@ export default class PeoplePage extends Component {
         )
 
         return (
-            <Row left={ itemList } right={ personDetails } />
+            <ErrorBoundary>
+                <Row left={ itemList } right={ personDetails } />
+            </ErrorBoundary>
         )
     }
 }
