3d6549bebe75a7cf8707fd6d3a12bbb06e7f9087
Maxattt <zhumyrtas@list.ru>
2025-03-26 08:23:14 +0500

    step 16 upd

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -2,17 +2,30 @@ import React, {Component} from "react"
  
  import Spinner from "../spinner"
  
- const withData = (View, getData) => {
-     return class extends Component {
-         state = {
-             data: null
-         }
+ const withData = (View) => {
+    return class extends Component {
+        state = {
+            data: null
+        }
  
-         componentDidMount() {
-             getData().then((data) => {
-                 this.setState({ data })
-             })
-         }
+        componentDidUpdate(prevProps) {
+            if (this.props.getData !== prevProps.getData) {
+                this.update()
+            }
+        }
+
+        componentDidMount() {
+            this.update()
+        }
+ 
+        update() {
+            this.props.getData()
+                .then((data) => {
+                    this.setState({
+                        data
+                    })
+                })
+        }
  
          render() {
             const { data } = this.state
